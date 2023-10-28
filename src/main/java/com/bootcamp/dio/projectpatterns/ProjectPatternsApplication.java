a96c7fbd5e1e3ec666b4a2ea591aaa40cdd35ad8
Victor Prudente <victorprudt@gmail.com>
2023-10-28 13:37:20 -0300

    First commit

---
+++ src/main/java/com/bootcamp/dio/projectpatterns/ProjectPatternsApplication.java
@@ -0,0 +1,13 @@
+package com.bootcamp.dio.projectpatterns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectPatternsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectPatternsApplication.class, args);
+	}
+
+}
