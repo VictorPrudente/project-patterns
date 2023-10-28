f1782b44cdd464bdad479c55e0e1fe01373c4b81
Victor Prudente <victorprudt@gmail.com>
2023-10-28 13:48:12 -0300

    Client and Address Class

---
+++ src/main/java/com/bootcamp/dio/projectpatterns/entities/Client.java
@@ -0,0 +1,50 @@
+package com.bootcamp.dio.projectpatterns.entities;
+
+
+import jakarta.persistence.*;
+
+@Entity
+public class Client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @Column(nullable = false)
+    private String firstName;
+    @Column(nullable = false)
+    private String lastName;
+    @ManyToOne
+    private Address address;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+}
